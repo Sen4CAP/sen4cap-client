bbe4c9f9ca7fad01ad1532dcc9a2e954116c2e45
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-09-11 15:33:07 +0200

    Testing new auth as of https://github.com/eo-tools/eozilla/pull/212

--- src/sen4cap_client/api.py
+++ src/sen4cap_client/api.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import Any
 
 from cuiman.api import AsyncClient, Client, ClientConfig
-from cuiman.api.auth import login
+from cuiman.api.auth import LoginAuthConfig
 from pydantic_settings import SettingsConfigDict
 
 from .opener import Sen4CAPJobResultsOpener
@@ -20,46 +20,24 @@ class Sen4CAPConfig(ClientConfig):
     )
 
 
-_CONFIG_BASE = Sen4CAPConfig(
-    api_url="http://localhost:8080/process/",
-    auth_url="http://localhost:8080/auth/login",
-    auth_type="login",
-    token_header="X-Auth-Token",
-    use_bearer=False,
-)
-_DEBUG = False
-
 Sen4CAPConfig.register_job_result_opener(Sen4CAPJobResultsOpener)
 
 ClientConfig.default_path = Path("~").expanduser() / ".sen4cap-client"
-ClientConfig.default_config = _CONFIG_BASE
-
-
-def _create_config(**config_overrides: Any) -> ClientConfig:
-    """
-    Create the S2GOS-specific configuration instance
-    from given configuration overrides.
-    """
-    # ClientConfig.create() will ready any previous configuration from
-    # ~/.sen4cap-client written by command "sen4cap-client configure":
-    config = ClientConfig.create(**config_overrides)
-    if config.auth_type != "login":
-        # already logged in
-        return config
-
-    # Login to get an (initial) access token and change auth_type to "token":
-    token = login(config)
-    config_dict = config.to_dict()
-    config_dict.update(auth_type="token", token=token)
-    return Sen4CAPConfig(**config_dict)
+ClientConfig.default_config = Sen4CAPConfig(
+    api_url="http://localhost:8080/process/",
+    auth=LoginAuthConfig(
+        login_url="http://localhost:8080/auth/login",
+        access_token_header="X-Auth-Token",
+    ),
+)
 
 
 def create_client(**config: Any) -> Client:
     """Create a synchronous Sen4CAP client from given configuration.
 
-    Provided configuration values, if any, override values
-    read from persistent configuration that were previously
-    written by the CLI command `sen4cap-client configure`.
+    Provided configuration values, if any, override Sen4CAP default
+    values or values read from persistent configuration that were
+    previously written by the CLI command `sen4cap-client configure`.
 
     Args:
         config: Configuration overrides. See
@@ -69,15 +47,15 @@ def create_client(**config: Any) -> Client:
         An instance of a synchronous cuiman client for Sen4CAP. See
         https://eo-tools.github.io/eozilla/cuiman/ for details.
     """
-    return Client(config=_create_config(**config), _debug=_DEBUG)
+    return Client(**config)
 
 
 def create_async_client(**config: Any) -> AsyncClient:
     """Create an asynchronous Sen4CAP client from given configuration.
 
-    Provided configuration values, if any, override values
-    read from persistent configuration that were previously
-    written by the CLI command `sen4cap-client configure`.
+    Provided configuration values, if any, override Sen4CAP default
+    values or values read from persistent configuration that were
+    previously written by the CLI command `sen4cap-client configure`.
 
     Args:
         config: Configuration overrides. See
@@ -87,7 +65,7 @@ def create_async_client(**config: Any) -> AsyncClient:
         An instance of an asynchronous cuiman client for Sen4CAP. See
         https://eo-tools.github.io/eozilla/cuiman/ for details.
     """
-    return AsyncClient(config=_create_config(**config), _debug=_DEBUG)
+    return AsyncClient(**config)
 
 
 __all__ = [
